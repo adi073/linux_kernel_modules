ad9f2c36fed98bb0afb07eca83866b758fe1faea
Aditya <bhushan.adit@gmail.com>
2024-01-14 10:41:54 +0100

    feat: simple LKM
    Signed-off-by: Aditya <bhushan.adit@gmail.com>

---
+++ init_module.c
@@ -0,0 +1,23 @@
+#include <linux/module.h>
+#include <linux/init.h>
+
+/*Meta Information*/
+MODULE_LICENSE("GPL")
+MODULE_AUTHOR("ADITYA BHUSHAN GNU/Linux")
+MODULE_DESCRIPTION("Hello World LKM")
+
+/* This function is called when the module is loaded in the kernel*/
+
+static int __init ModuleInit(void){
+    printk("Hello Kernel !\n");
+    return 0;
+}
+
+/* This function is called when the module is exiting the kernel*/
+static void __exit ModuleExit(void){
+    printk("Bye Kernel\n");
+    return 0;
+}
+
+module_init (ModuleInit);
+module_exit (ModuleExit);
